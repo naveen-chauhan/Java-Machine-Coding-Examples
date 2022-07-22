7f5bb98b8b510c81d406c7b7e02c5469c9d82e01
Naveen  Chauhan <naveenchauhan@Naveens-MacBook-Air.local>
2022-07-22 18:19:44 +0530

    More Cleaner Implementation;

---
+++ src/main/java/lruBasedCache/evictionPolicy/LRUEvictionPolicy.java
@@ -0,0 +1,45 @@
+package lruBasedCache.evictionPolicy;
+
+import lruBasedCache.comparator.SortbyFreq;
+import lruBasedCache.models.LRUEvictionData;
+
+import java.util.Optional;
+import java.util.PriorityQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author naveen.chauhan on 22/07/22
+ */
+public class LRUEvictionPolicy {
+	PriorityQueue<LRUEvictionData> lru;
+	AtomicInteger timeValue;
+
+
+	public void init() {
+		lru = new PriorityQueue<>(new SortbyFreq());
+		timeValue = new AtomicInteger(1);
+	}
+
+
+	public void add(String key) {
+		if (lru.isEmpty()) {
+			lru.add(new LRUEvictionData(key, timeValue.incrementAndGet()));
+			return;
+		}
+
+		Optional<LRUEvictionData> lruEvictionDataOptional = lru.stream().filter(data -> data.getKey().equals(key)).findAny();
+
+		if (lruEvictionDataOptional.isPresent()) {
+			lru.remove(lruEvictionDataOptional.get());
+			lru.add(new LRUEvictionData(lruEvictionDataOptional.get().getKey(), timeValue.incrementAndGet()));
+		} else {
+			lru.add(new LRUEvictionData(key, timeValue.incrementAndGet()));
+		}
+	}
+
+
+	public String evict() {
+		LRUEvictionData evictionData = lru.poll();
+		return evictionData.getKey();
+	}
+}
