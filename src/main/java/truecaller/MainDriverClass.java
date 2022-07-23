d8f681044ad685d2e871b4a0df1160d2f2223b52
Naveen  Chauhan <naveenchauhan@Naveens-MacBook-Air.local>
2022-07-23 18:37:50 +0530

    Truecaller Implementation Part 2

--- src/main/java/truecaller/MainDriverClass.java
+++ src/main/java/truecaller/MainDriverClass.java
@@ -1,7 +1,15 @@
 package truecaller;
 
+import truecaller.services.GlobalSvc;
+
 /**
  * @author naveen.chauhan on 23/07/22
  */
 public class MainDriverClass {
+	public static void main(String[] args) {
+		//No Implementation here.
+		//Todo: Add commands from the problem statements
+
+		GlobalSvc globalSvc = new GlobalSvc();
+	}
 }
